edc5b6b0a03a5c8e87a8396d915483953caccfec
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2026-03-01 01:52:48 -0300

    feat: adicionando cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Security Headers & CORS
+  app.use(helmet());
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.set('trust proxy', 'loopback');
   await app.listen(process.env.PORT ?? 3001);
 }
